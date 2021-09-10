1321fabc131ac6925dc1b188a4a3124f2ba467b8
armenter66 <87765970+armenter66@users.noreply.github.com>
2021-09-10 17:15:01 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+/* 
+    $('.testemonials__slider').slick({
+        speed: 300,
+        autoplay: true,
+        adaptiveHeight: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        autoplaySpeed: 2000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+        responsive: [
+            {
+            breakpoint: 992,
+            settings: {
+                dots: true,
+                arrows: false
+                }
+            },
+        ]
+      });
+     */
